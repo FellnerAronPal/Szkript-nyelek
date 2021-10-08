b67eb7e8066841783de1af18efa0e39733b9906c
AronFellner <fap7td97>
2021-10-08 12:17:10 +0200

    feat(hangment): add logger

--- 02/hangman.py
+++ 02/hangman.py
@@ -1,7 +1,12 @@
 from typing import List
+import logging
 
 
 def get_all_index(text: str, word: str) -> List[int]:
+    if type(text) != str:
+        raise TypeError
+    if len(text) != 1:
+        raise ValueError
     # return [i for i, letter in enumerate(word) if letter == text]
     found_index = []
     for i in range(len(word)):
@@ -9,22 +14,23 @@ def get_all_index(text: str, word: str) -> List[int]:
             found_index.append(i)
     return found_index
 
+
 # 1. l -> xxllx
 # 2. e -> xellx
 
 
 def get_new_masked_word(masked_word: str, word: str, indexes: List[int]) -> str:
-    new_masked_word = list(masked_word)
+    new_masked_word_list = list(masked_word)
 
     for step in range(len(word)):
         if step in indexes:
-            new_masked_word[step] = word[step]
+            new_masked_word_list[step] = word[step]
 
-    return "".join(new_masked_word)
+    return "".join(new_masked_word_list)
 
 
 def lost() -> None:
-    print("Sorry , U have lost")
+    print("Sorry, U have lost")
     exit(1)
 
 
@@ -45,10 +51,19 @@ def handle_final_guess(text: str, word: str) -> None:
         lost()
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
+    FORMAT = '%(asctime)s - %(levelname)s - %(message)s'
+    logging.basicConfig(filename='hangman.log', filemode='w',level=logging.DEBUG, format=FORMAT)
+    LOGGER = logging.getLogger()
+    # LOGGER.debug('Debug')
+    # LOGGER.info('Info')
+    # LOGGER.warning('Warning')
+    # LOGGER.error('Error')
+    # LOGGER.critical('Critical')
+
     word = "hello"
     MAX_GUESS = 3
-    print(f"Hangman application. Try to guess my word. You have {MAX_GUESS}")
+    print(f"hangman application. Try to guess my word. You have {MAX_GUESS} chance.")
 
     masked_word = len(word) * "x"
 
@@ -56,11 +71,18 @@ if __name__ == "__main__":
         print(f"{step + 1}. step, pls give me a letter")
         text = input()
 
+        LOGGER.debug(f'Input: {text}, Step {step}')
+
         is_new_guess = len(text) == 1
         if is_new_guess:
-            new_guess(text, word, masked_word)
+            LOGGER.info(f'New Guess: {text}')
+            masked_word = new_guess(text, word, masked_word)
+            print(f"masked_word {masked_word}")
             if masked_word == word:
+                LOGGER.info(f'Win')
                 win()
         else:
+            LOGGER.info(f'Final Guess: {text}')
             handle_final_guess(text, word)
+
     lost()
